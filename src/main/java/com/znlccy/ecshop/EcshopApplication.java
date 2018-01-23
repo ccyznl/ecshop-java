65de4cc108487389c544e05bb54e80beb739cb0e
ccyznl <2845843140@qq.com>
2018-01-23 23:06:14 +0800

    添加Readme文件

--- src/main/java/com/znlccy/ecshop/EcshopApplication.java
+++ src/main/java/com/znlccy/ecshop/EcshopApplication.java
@@ -2,9 +2,19 @@ package com.znlccy.ecshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EcshopApplication {
+
+	@RequestMapping(value = "/")
+	@ResponseBody
+	public String index() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcshopApplication.class, args);
